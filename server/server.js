1a1c21340e696ba7d6635084e0e5648fda31f397
macbook <shkelqim__99@muzikpapare.com>
2023-03-09 15:51:21 +0100

    server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+    "mongodb+srv://qimi:qimibe123@amazon-clone.c8xzpsc.mongodb.net/?retryWrites=true&w=majority", 
+    {useNewUrlParser: true, useUnifiedTopology: true,}, 
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connesct to the database");
+        }
+    }
+);
 
 //Middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Retrive data from the server
 app.get("/", (req, res) => {
     res.json("Hello amazon clone");
 });
-
+// Post - send data from  front to backend
 app.post("/", (req, res) =>{
     console.log(req.body.name);
 });
